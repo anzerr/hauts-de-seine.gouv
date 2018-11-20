d2120999421a15f9e24ee824d94066fec49e5263
anzerr <ryan.anzer@epitech.eu>
2018-11-20 11:48:23 +0100

    fast save

--- test.js
+++ test.js
@@ -1,33 +1,33 @@
-var http = require("http");
+let http = require('http');
 
-var options = {
-  "method": "GET",
-  "hostname": [
-    "www",
-    "hauts-de-seine",
-    "gouv",
-    "fr"
-  ].join('.'),
-  "path": '/' + ([
-    "booking",
-    "create",
-    "4462",
-    ""
-  ].join('/')),
-  "headers": {}
+let options = {
+	method: 'GET',
+	hostname: [
+		'www',
+		'hauts-de-seine',
+		'gouv',
+		'fr'
+	].join('.'),
+	path: '/' + ([
+		'booking',
+		'create',
+		'4462',
+		''
+	].join('/')),
+	headers: {}
 };
 console.log(options);
-var req = http.request(options, function (res) {
-  var chunks = [];
+let req = http.request(options, (res) => {
+	let chunks = [];
 
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
+	res.on('data', (chunk) => {
+		chunks.push(chunk);
+	});
 
-  res.on("end", function () {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
+	res.on('end', () => {
+		let body = Buffer.concat(chunks);
+		console.log(body.toString());
+	});
 });
 
 req.end();
